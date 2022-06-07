b6241c18bd8e295ec6dc8dffcb2e326b728f9d54
Micho-PC <michelhdd27@gmail.com>
2022-06-08 00:12:03 +0300

    created add resto api

--- app/Http/Controllers/RestoController.php
+++ app/Http/Controllers/RestoController.php
@@ -4,7 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//Add the model
+use App\Models\Restaurant;
+
 class RestoController extends Controller
 {
-    
+    //Add a restaurant (post)
+    public function addResto(Request $request){
+        $resto = new Restaurant;
+        $resto->name = $request->name;
+        $resto->location = $request->location;
+        $resto->avg_cost = $request->avg_cost;
+        $resto->category = $request->category;
+        $resto->description = $request->description;
+        $resto->save();
+        
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/add_resto', [RestoController::class, 'addResto']);
 
 
 
