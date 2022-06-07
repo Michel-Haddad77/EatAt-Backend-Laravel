4331fd6f0d5afd6ce354b255856be18440df86c5
Micho-PC <michelhdd27@gmail.com>
2022-06-07 21:42:57 +0300

    created migrations to create tables in db

---
+++ database/migrations/2022_06_07_000000_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->text('review')->unique();
+            $table->integer('rating');
+            $table->foreignId('user_id');
+            $table->foreignId('restaurant_id');
+            $table->tinyInteger('is_pending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
--- database/migrations/2022_06_07_000000_create_users_table.php
+++ database/migrations/2022_06_07_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->tinyInteger('type');
+            $table->string('picture')->nullable();
         });
     }
 
